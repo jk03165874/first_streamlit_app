f224ac5f591ca4bbad005f1ad4af9aeb9be6b7dc
jk03165874 <139709059+jk03165874@users.noreply.github.com>
2023-08-06 15:38:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,9 +27,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 #create the repeatable code block (called a function)
 def get_fruityvice_data(this_fruit_choice):
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      streamlit.dataframe(fruityvice_normalized)
+      return fruityvice_normalized
   
 #New Section to display fruityvice api response//Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
 streamlit.header("Fruityvice Fruit Advice!")
